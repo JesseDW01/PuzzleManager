3642e01962d397034eb21e290261efc0dc91cb15
Jesse De Weerdt <jesse.de-weerdt@be.abb.com>
2025-01-05 16:06:34 +0100

    Simplify auto mapper

--- PuzzleManager.Services/Mappings/PuzzleMappingProfile.cs
+++ PuzzleManager.Services/Mappings/PuzzleMappingProfile.cs
@@ -13,13 +13,9 @@ namespace PuzzleManager.Services.Mappings
 		{
 			// Map JVHOnlinePuzzleDto to Puzzle domain model
 			CreateMap<JVHOnlinePuzzleDto, Puzzle>()
+				// properties with the same name are automatically mapped
 				.ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Title))
 				.ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.FullTitle))
-				.ForMember(dest => dest.PieceCount, opt => opt.MapFrom(src => src.PieceCount))
-				.ForMember(dest => dest.ProductUrl, opt => opt.MapFrom(src => src.ProductUrl))
-				.ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl))
-				.ForMember(dest => dest.Year, opt => opt.MapFrom(src => src.Year))
-				.ForMember(dest => dest.ArticleNumber, opt => opt.MapFrom(src => src.ArticleNumber))
 
 				// Ignore following proprties as tyet will be assigned trough services surrounding
 				.ForMember(dest => dest.PuzzleId, opt => opt.Ignore())
